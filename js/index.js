143f79184cfb3ddb78152b0a1d506958c240bf95
Markus <70133910+MarkusG94@users.noreply.github.com>
2020-12-10 21:00:17 +0100

    added some comments and css

--- js/index.js
+++ js/index.js
@@ -6,40 +6,43 @@ let btn = document.getElementById('btn');
 let toggle = document.getElementById('toggleBtn');
 let medium = '_b';
 let small = '_m';
-let item = '';
+let page = '&per_page=12';
+let sort = '&sort=relevance';
+// let item = '';
 
 
 async function getData(query) {
 
     document.getElementById('render_images').innerHTML = null;
-    const response = await fetch(`${url}&text=${query}&api_key=${key}&per_page=12&sort=relevance&extras=url_sq,url_m,description&format=json&nojsoncallback=?`)
+    const response = await fetch(`${url}&text=${query}&api_key=${key}${page}${sort}&extras=url_sq,url_m,description&format=json&nojsoncallback=?`)
     const data = await response.json();
     console.log(data);
     getImage(data);
 };
 
 
-
+//Eventlyssnare med click på knappen "search".
 btn.addEventListener('click', () => {
     getData(search.value);
 });
 
+// Gör att du kan söka med att trycka på enter
 search.addEventListener("keydown", function(e) {
     if (e.code === "Enter") {
         getData(search.value);
     }
   });
 
-
-
 function getImage(data) {
     let images = document.getElementById('render_images');
 
+    // if satsen nedan gör att om det vi söker på inte finns så skrivs det ut i HTML "hittades ej".
     if (!data.photos.photo.length) {
-        images.innerHTML = 'Hittade inte ' + search.value;
+        images.innerHTML = `No match found from your input "${search.value}", please try again!`
         return;
     }
-   
+   // Nedan kör vi forEach så att vi kan göra en dynamisk URL med hjälp av parameter.
+   // Vi skapar även en li i vår UL för varje bild vi får fram.
     data.photos.photo.forEach(element => {
         let item = document.createElement('li')
         item.style.listStyle = 'none';
@@ -49,7 +52,3 @@ function getImage(data) {
     });
 }
 
-/*  1. Gör en async function,
-    2. Gör en knapp som kör fetchen
-    3. loopa ut Farm, ID, Server, Secret. 
-*/
