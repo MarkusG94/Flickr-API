f7d699ad05d305d2aa09e85ce2fe18a63778fa2d
Markus <70133910+MarkusG94@users.noreply.github.com>
2020-12-14 23:18:55 +0100

    last finish, lightbox. style. comments

--- js/index.js
+++ js/index.js
@@ -10,7 +10,6 @@ let perPage = '&per_page=12';
 let page = 1;
 let sort = '&sort=relevance';
 let totalPages;
-// let item = '';
 let setPageNmbr = document.getElementById('set-page-number');
 let prev = document.getElementById('prev');
 let next = document.getElementById('next');
@@ -20,16 +19,22 @@ async function getData(query, page) {
     document.getElementById('render_images').innerHTML = null;
     const response = await fetch(`${url}&text=${query}&api_key=${key}${perPage}&page=${page}${sort}&extras=description&format=json&nojsoncallback=?`)
     const data = await response.json();
-    totalPages = data.photos.pages;
+
+    if (data.photos.pages >= 5) {
+        totalPages = 5;
+    } else {
+        totalPages = data.photos.pages;
+    }
 
     console.log(data);
     updatePageCounter(page, totalPages);
     getImage(data);
 };
 
+// visar vilken sida du är på
 function updatePageCounter(currentPage, totalPage) {
     let html = document.getElementById('page-count');
-    let text = `Sida ${currentPage} av ${totalPage}`
+    let text = `Page ${currentPage} of ${totalPage}`
 
     html.innerHTML = text;
 }
@@ -37,38 +42,47 @@ function updatePageCounter(currentPage, totalPage) {
 
 //Eventlyssnare med click på knappen "search".
 btn.addEventListener('click', () => {
+    page = 1;
+    setPageNmbr.value = '';
     getData(search.value, page);
 });
 
+//trycker du på next kommer du till nästa sida
 next.addEventListener('click', () => {
 
     if (page >= 1 && page < totalPages) {
         page = page + 1
+        setPageNmbr.value = '';
         getData(search.value, page);
     }
 });
 
+//samma som ovan fast du går bakåt ett steg
 prev.addEventListener('click', () => {
     if (page >= 2) {
         page = page - 1
+        setPageNmbr.value = '';
         getData(search.value, page);
     }
 });
 
-
+//Denna gör att när du trycker på enter söker du efter sida i sökfältet
 setPageNmbr.addEventListener("keydown", function (e) {
     if (e.code === "Enter") {
         let pageNumber = parseInt(e.currentTarget.value)
         page = parseInt(e.currentTarget.value);
-
-        getData(search.value, pageNumber);
+        if (page >= 1 && page <= totalPages) {
+            getData(search.value, pageNumber);
+        }
     }
 });
 
 
-// Gör att du kan söka med att trycka på enter
+// Gör att du kan söka med att trycka på enter om man vill
 search.addEventListener("keydown", function (e) {
     if (e.code === "Enter") {
+        page = 1;
+        setPageNmbr.value = '';
         getData(search.value, page);
     }
 });
@@ -87,8 +101,34 @@ function getImage(data) {
         let item = document.createElement('li')
         item.style.listStyle = 'none';
         item.classList.add('gridPic');
-        item.innerHTML = (`<img src="https://farm${element.farm}.staticflickr.com/${element.server}/${element.id}_${element.secret}${small}.jpg"/>`);
+        item.innerHTML = (`<img data-high_res="https://farm${element.farm}.staticflickr.com/${element.server}/${element.id}_${element.secret}${medium}.jpg" data-description='${element.description._content}' class="flickr-image" src="https://farm${element.farm}.staticflickr.com/${element.server}/${element.id}_${element.secret}${small}.jpg"/>`);
         images.appendChild(item);
     });
+
+    initLightbox();
 }
 
+// Lightbox
+var modal = document.getElementById("myModal");
+var closeButton = document.getElementsByClassName("close")[0];
+closeButton.onclick = function () {
+    modal.style.display = "none";
+}
+
+function initLightbox() {
+    let images = document.querySelectorAll(".flickr-image");
+    images.forEach((el) => { //loopar igenom alla bilder och skapar ett clickevent
+        // som sätter den clickade bildens url till lightboxens
+        //img-tag (highres med data-attribut).
+        el.addEventListener('click', (e) => {
+            modal.style.display = "block";
+
+            let clickedImage = e.currentTarget;
+            let modalImg = document.getElementById("modal-img");
+            let captionText = document.getElementById("caption");
+            //
+            modalImg.src = clickedImage.dataset.high_res;
+            captionText.innerHTML = clickedImage.dataset.description;
+        });
+    });
+}
